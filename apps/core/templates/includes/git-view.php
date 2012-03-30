7bfdb23096e5faa356c795b3ce912ee9dec7d3a5
James Thompson <keiouu@gmail.com>
2012-03-30 01:49:46 +0100

    Yay

--- apps/core/templates/includes/git-view.php
+++ apps/core/templates/includes/git-view.php
@@ -7,9 +7,9 @@ if ($git_data) {
 	$dateField = new DateTimeField();
 	$dateField->set_value(date("Y-m-d H:i:s", $git_data["date"]));
 	$gravatar = md5(strtolower(trim($git_data["email"])));
-	print '<thead><tr><th>' . $git_data["message"] . '</th></tr></thead>';
+	print '<thead><tr><th><a href="'.$request->fullPath.'git/'.$git_data["hash"].'/">' . $git_data["message"] . '</a></th></tr></thead>';
 	print '<tbody><tr><td><span class="author"><img src="http://www.gravatar.com/avatar/'.$gravatar.'?s=22" alt="Profile Image" />' . $git_data["author"] . '</span> <span class="date">(' . $dateField->get_readable_value() . ')</span>';
-	print '<a href="'.$request->fullPath.'git/'.$git_data["hash"].'/" class="hash">' . ellipsize($git_data["hash"], 13) . '</a></td></tr></tbody>';
+	print '<a href="'.$request->fullPath.'git/'.$git_data["hash"].'/" class="hash">' . ellipsize($git_data["hash"], 13) . '<i class="icon-share-alt"></i></a></td></tr></tbody>';
 } else {
 	print '<td>No commits!</td>';
 }
