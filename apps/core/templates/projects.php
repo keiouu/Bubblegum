0e3452c6862da13f9a326b7e15d96bba4ca9ae46
James Thompson <keiouu@gmail.com>
2012-04-26 23:43:33 +0100

    Dontshow get involved box if no projects in it

--- apps/core/templates/projects.php
+++ apps/core/templates/projects.php
@@ -9,17 +9,19 @@
 </div>
 
 <?php
-print '<div class="row-fluid well"><h3>Get involved in some other projects..</h3><br />';
 
 // Show some random public projects to "get involved in"
 $count = 0;
 $projects = Project::find(array("public" => true))->shuffle();
+
+$p_output = '<div class="row-fluid well"><h3>Get involved in some other projects..</h3><br />';
+
 foreach ($projects as $project) {
 	if (isset($shown[$project->pk]))
 		continue;
 	if ($count >= 5)
 		break;
-	print '<div class="span2">
+	$p_output .= '<div class="span2">
 		<h4>'.$project->name.'</h4>
 		<p>'.$project->description.'</p>
 		<p><a class="btn" href="'.home_url.'projects/'.$project->pk.'/">Get Involved &raquo;</a></p>
@@ -28,7 +30,11 @@ foreach ($projects as $project) {
 	$shown[$project->pk] = true;
 }
 
-print '</div>';
+$p_output .= '</div>';
+
+if ($count > 0)
+	print $p_output;
+
 ?>
 {% endblock body %}
 
