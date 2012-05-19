8d3e081e0664175181588e25ae5feaa4e8dc9fa2
James Thompson <keiouu@gmail.com>
2012-05-19 20:24:36 +0100

    Roll in Bubblegum branding

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * TNet Version 2.0_dev
+ * Bubblegum Version 1.0_dev
  * 
  * For installation instructions see README
  * For license information please see LICENSE
@@ -22,13 +22,13 @@ define("media_url", home_url . "media/");
 define("admin_url", home_url . "admin/");
 
 // Best change these!
-define("project_name", 'TNet');                    // The name of your project
+define("project_name", 'Bubblegum');                    // The name of your project
 define("site_logo", media_url . "images/logo.png");   // The URL to a logo for your project
 
 if (PHP_SAPI !== 'cli')
 	define("site_url", $_SERVER['SERVER_ADDR']);          // The URL of your website.
 else
-	define("site_url", "CLI"); // TODO - this should be hardcoded anyway
+	define("site_url", "http://bubblegum.flamehost.org");
 
 if (!file_exists(home_dir . "config.php"))
 	die("You must supply a config file!");
