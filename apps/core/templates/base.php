de8a8737fb2c044721b5169d7a29651924f50c3e
James Thompson <keiouu@gmail.com>
2012-06-01 20:23:46 +0100

    Fixes

--- apps/core/templates/base.php
+++ apps/core/templates/base.php
@@ -22,13 +22,9 @@ require_once(home_dir . "apps/core/models.php");
     </style>
     <link href="{{home_url}}apps/core/media/css/bootstrap-responsive.css" rel="stylesheet">
     <link href="{{home_url}}apps/core/media/css/style.css" rel="stylesheet">
-    {% block style %}
-    {% endblock %}
-    
+    {% block style %}{% endblock %}
 	{% jsvars %}
-	 
-    {% block head %}
-    {% endblock %}
+    {% block head %}{% endblock %}
     
     <!--[if lt IE 9]>
       <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
--- apps/core/templates/dashboard.php
+++ apps/core/templates/dashboard.php
@@ -13,48 +13,52 @@
       <div style="height: 25px;width: 100%;"></div>
       
       <div class="span3 draggables c1">
-      	<h3>Product Backlog</h3>
+      	<h3>Backlog</h3>
       	<div class="inner">
-	      	<div class="draggable btn btn-primary">
-		      	<p class="title">Do this and this and this</p>
-		      	<p class="author">James Thompson</p>
-	      	</div>
-	      	<div class="draggable btn btn-primary">
-		      	<p class="title">Task1</p>
-		      	<p class="author">James Thompson</p>
-	      	</div>
+      		<?php
+      		require_once(home_dir . "apps/core/models.php");
+      		$tasks = Task_Link::find(array("assignee" => $request->user));
+			foreach ($tasks as $task) {
+				if ($task->task->status == $task->task->_status["New"] || $task->task->status == $task->task->_status["Confirmed"]) {
+					print '	<div class="draggable btn btn-primary">
+				      			<p class="title">'.$task->task->name.'</p>
+				      			<p class="author">'.$task->task->updated.'</p>
+			      			</div>';
+				}
+			}
+      		?>
       	</div>
       </div>
 
       <div class="span3 draggables c1">
-      	<h3>Sprint Backlog</h3>
+      	<h3>In Progress</h3>
       	<div class="inner">
-	      	<div class="draggable btn btn-danger">
-		      	<p class="title">Task2</p>
-		      	<p class="author">James Thompson</p>
-	      	</div>
-	      	<div class="draggable btn btn-primary">
-		      	<p class="title">Task3</p>
-		      	<p class="author">James Thompson</p>
-	      	</div>
+      		<?php
+			foreach ($tasks as $task) {
+				if ($task->task->status == $task->task->_status["In Progress"] || $task->task->status == $task->task->_status["Testing"] || $task->task->status == $task->task->_status["Awaiting Feedback"]) {
+					print '	<div class="draggable btn btn-primary">
+						      	<p class="title">'.$task->task->name.'</p>
+						      	<p class="author">'.$task->task->updated.'</p>
+					      	</div>';
+				}
+			}
+      		?>
       	</div>
       </div>
       
       <div class="span3 draggables c1">
       	<h3>Complete</h3>
       	<div class="inner">
-	      	<div class="draggable btn btn-warning">
-		      	<p class="title">Task4</p>
-		      	<p class="author">James Thompson</p>
-	      	</div>
-	      	<div class="draggable btn btn-success">
-		      	<p class="title">Task5</p>
-		      	<p class="author">James Thompson</p>
-	      	</div>
-	      	<div class="draggable btn btn-success">
-		      	<p class="title">Task 6 - Do blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
-		      	<p class="author">James Thompson</p>
-	      	</div>
+      		<?php
+			foreach ($tasks as $task) {
+				if ($task->task->status == $task->task->_status["Complete"]) {
+					print '	<div class="draggable btn btn-primary">
+				      			<p class="title">'.$task->task->name.'</p>
+				      			<p class="author">'.$task->task->updated.'</p>
+			      			</div>';
+				}
+			}
+      		?>
       	</div>
       </div>
     
