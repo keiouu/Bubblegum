f05c6acbf1afa9cb823d605eb00b00b861d709c5
James Thompson <keiouu@gmail.com>
2012-07-27 20:56:29 +0100

    Cleanup home page

--- apps/core/templates/index.php
+++ apps/core/templates/index.php
@@ -5,11 +5,7 @@
 	<div class="page-header">
 		<h1>Welcome to {{project_name}}!</h1>
 	</div>
-	<p class="add-links">
-		<a href="{{home_url}}projects/new/">Start new Project &raquo;</a>
-		<a href="{{home_url}}projects/">View my Projects &raquo;</a>
-	</p>
-	<hr />
+	
 	<div class="accordion-heading">
 		<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#tasksCollapse">
 			<h4><i class="icon-check"></i> Your Tasks</h4>
